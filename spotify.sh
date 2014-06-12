b6c18593cc74485fcb5e663350dc10a3cdc04451
Frank Lyder Bredland <fl@nyggen.com>
2014-06-12 08:04:36 +0200

    Added check if spotify is running

--- spotify.sh
+++ spotify.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-name=$(osascript -e 'tell application "Spotify" to name of current track as string')
-artist=$(osascript -e 'tell application "Spotify" to artist of current track as string')
-
-echo "$name - $artist"
-
+if ps aux | grep "spotify" > /dev/null
+then
+	name=$(osascript -e 'tell application "Spotify" to name of current track as string')
+	artist=$(osascript -e 'tell application "Spotify" to artist of current track as string')
+	
+	echo "$name - $artist"
+fi
