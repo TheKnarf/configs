dab4c59bb91e2179e1d1a17f8b52f9ff80b24bd0
Frank Lyder Bredland <fl@nyggen.com>
2016-11-14 21:07:12 +0100

    Added a command for setting screenshot folder

--- osx.sh
+++ osx.sh
@@ -2,3 +2,7 @@
 
 # Disable the "Are you sure you want to open this application?" dialog
 defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+# Set Screenshot folder to ~/Downloads
+defaults write com.apple.screencapture location ~/Downloads
+killall SystemUIServer
