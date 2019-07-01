b78258ce584b3e23fc0849dda7e7513c6faf86c7
Frank Lyder Bredland <fl@nyggen.com>
2019-07-01 14:09:11 +0200

    Brew setup

--- macos-setup.sh
+++ macos-setup.sh
@@ -13,8 +13,65 @@ command -v brew >/dev/null 2>&1 || { \
 	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"; \
 }
 
+brew_check_if_installed () {
+	if brew ls --versions $1 > /dev/null; then
+		echo "$1 allready installed"
+	else
+		echo "$1 is not installed with brew"
+		read -p "Install it? [yN] " -n 1 -r
+		if [[ $REPLY =~ ^[Yy]$ ]]; then
+			echo
+			echo "Installing $1"
+			brew install $1
+		fi
+	fi
+}
+
+brew_cask_check_if_installed () {
+	if brew cask ls --versions $1 > /dev/null; then
+		echo "$1 allready installed"
+	else
+		echo "$1 is not installed with brew cask"
+		read -p "Install it? [yN] " -n 1 -r
+		if [[ $REPLY =~ ^[Yy]$ ]]; then
+			echo
+			echo "Installing $1"
+			brew cask install $1
+		fi
+	fi
+}
+
+
+brew_check_if_installed git
+
+brew_cask_check_if_installed iTerm2
+
 echo 'Specify the preferences directory for iTerm2'
 defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.iterm2_profile"
 
 echo 'Tell iTerm2 to use the custom preferences in the directory'
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+
+brew_cask_check_if_installed google-chrome
+
+# Enviromet
+brew_check_if_installed z
+brew_check_if_installed tmux
+brew_check_if_installed fzf
+brew_check_if_installed wget
+brew_check_if_installed tree
+brew_check_if_installed watch
+
+# Pandoc
+brew_check_if_installed pandoc
+brew_check_if_installed pandoc-citeproc
+brew_check_if_installed pandoc-crossref
+
+# Node
+brew_check_if_installed node
+
+# C / C++
+brew_check_if_installed cmake
+
+# Other
+brew_check_if_installed ansible
