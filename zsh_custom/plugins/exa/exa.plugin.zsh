370871899b5f50aeede4a925185ac5f297b1c96b
Frank Lyder Bredland <fl@nyggen.com>
2023-06-14 16:32:22 +0200

    Only remap `ls` to `exa` if `exa` is installed

--- zsh_custom/plugins/exa/exa.plugin.zsh
+++ zsh_custom/plugins/exa/exa.plugin.zsh
@@ -1,3 +1,8 @@
-alias ls='exa --grid --color auto --icons --sort=type'
-alias ll='exa --long --color always --icons --sort=type'
-alias la='exa --long --all --color auto --icons --sort=type'
+# If the exa command is installed
+# then update the `ls`, `ll`, and `la` commands
+if command -v exa &> /dev/null
+then
+	alias ls='exa --grid --color auto --icons --sort=type'
+	alias ll='exa --long --color always --icons --sort=type'
+	alias la='exa --long --all --color auto --icons --sort=type'
+fi
