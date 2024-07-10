6d3bcea5cbd633ea779d0f67cc5e0c81ce199161
Frank Lyder Bredland <fl@nyggen.com>
2024-07-10 12:26:29 +0200

    NixOS's home-manager, organized and wayland setup
    - Changed the shebang for `setup` to work on nix
    - Organized home-manager into multiple nix files
    - Initial hyprland setup for wayland

---
+++ os/nixos/home-manager/env.nix
@@ -0,0 +1,41 @@
+{ config, pkgs, ... }:
+
+{
+  home.packages = [
+		pkgs.zsh
+
+		pkgs.git
+
+		pkgs.kitty
+
+		pkgs.tmux
+		pkgs.fzf
+		pkgs.fd
+		pkgs.ripgrep
+		pkgs.wget
+		pkgs.tree
+
+		pkgs.htop
+		pkgs.jq
+		pkgs.cloc
+
+		pkgs.direnv
+		pkgs.git-annex
+		pkgs.eza
+		pkgs.bat
+
+		pkgs.gh
+		pkgs.yazi
+
+		# Editors
+		pkgs.vim
+		pkgs.neovim
+		pkgs.helix
+
+		# Programming
+		pkgs.fnm # Fast and simple Node.js version manager
+		pkgs.bun
+		pkgs.rustup
+		pkgs.go
+  ];
+}
